ce849bf3b943083389e26695e03e584f1608c189
AsamPrabhakarReddy <94506736+AsamPrabhakarReddy@users.noreply.github.com>
2024-09-01 21:21:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1> Hello deploying with ci/cd </h1>
+        <h1> Hello deploying with ci/cd with vinay</h1>
     </div>
   );
 }
