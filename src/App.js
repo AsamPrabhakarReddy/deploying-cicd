c41510cf1b67c6b24334302a02b3e643cdf2e656
AsamPrabhakarReddy <94506736+AsamPrabhakarReddy@users.noreply.github.com>
2024-09-03 10:38:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1> Hello deploying with ci/cd with vinay and thaha, Bala</h1>
+        <h1> Hello deploying with ci/cd with purnesh</h1>
     </div>
   );
 }
